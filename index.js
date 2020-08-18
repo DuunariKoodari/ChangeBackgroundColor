ff0385ba434f44dc988cc72bade0f0858db22140
Rasmus G <rasse91@gmail.com>
2020-08-18 15:00:53 +0300

    commit

---
+++ index.js
@@ -0,0 +1,14 @@
+//muuttujat
+const ColorButton = document.querySelector('button');
+const body = document.querySelector('body');
+const colors = ['red', 'blue', 'yellow', 'pink', 'green', 'purple'];
+
+ColorButton.addEventListener('click', changeBackgroundColor);
+
+//Värin vaihto funktio
+function changeBackgroundColor() {
+  let colorIndex = Math.floor(Math.random() * colors.length);
+  body.style.backgroundColor = colors[colorIndex];
+  console.log(colorIndex);
+}
+console.log(Math.floor(Math.random() * colors.length));
